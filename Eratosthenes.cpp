d34fca0f0f99702a358297d8918a2c4a4e49aa96
Abdullah Siddique <137615484+Abdullah-Siddique@users.noreply.github.com>
2026-09-22 10:57:09 +0600

    Update Eratosthenes.cpp

--- Eratosthenes.cpp
+++ Eratosthenes.cpp
@@ -1,20 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
     ll n;
-    cin>>n;
-    vector<bool>f(n+1, true);
+    cin >> n;
+    vector<bool> f(n + 1, true);
     f[0] = f[1] = false; 
+    
     for(ll i = 2; i * i <= n; i++){
-        for(ll j = 2 * i; j <= n; j += i){
-            if(f[j]) f[j] = false;
+        if (f[i]) {
+            for(ll j = i * i; j <= n; j += i){
+                f[j] = false;
+            }
         }
     }
-    cout<<"Number of prime numbers: "<<count(f.begin(), f.end(), true)<<"\n";
-    for(ll i=0; i<n+1; i++){
-        if(f[i])cout<<i<<" ";
+    
+    cout << "Number of prime numbers: " << count(f.begin(), f.end(), true) << "\n";
+    for(ll i = 0; i <= n; i++){
+        if(f[i]) cout << i << " ";
     }
 }
