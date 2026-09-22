a670c166e2dd4398463beacdaaf2a49f7bd9b26b
Abdullah Siddique <137615484+Abdullah-Siddique@users.noreply.github.com>
2026-09-22 11:13:52 +0600

    Refactor modular inverse calculation functions

--- MMI.cpp
+++ MMI.cpp
@@ -1,26 +1,27 @@
 /*
-ax + mp = gcd(a, m) = 1
+ax + by = gcd(a, b) = 1
 */
 
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
 
-ll eea(ll &x, ll &y, ll a, ll b){
+ll eea(ll a, ll b, ll &x, ll &y){
     if(b == 0){
-        y = 0;
         x = 1;
+        y = 0;
         return a;
     }
-    ll x1 = 0, y1 = 0;
-    ll gcd = eea(x1, y1, b, a % b);
+    ll x1, y1;
+    ll gcd = eea(b, a % b, x1, y1);
     x = y1;
-    y = (x1 - (a / b) *  y1);
+    y = x1 - (a / b) * y1;
     return gcd;
 }
 
-ll modin(ll a, ll m, ll &x, ll &p){
-    ll gcd = eea(x, p, a, m);
+ll modin(ll a, ll m){
+    ll x, y;
+    ll gcd = eea(a, m, x, y);
     if(gcd != 1) return -1;
     return (x % m + m) % m;
 }
@@ -28,9 +29,16 @@ ll modin(ll a, ll m, ll &x, ll &p){
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
-    ll a, m, x, p;
+    
+    ll a, m;
     cin >> a >> m;
-    cout << "MMI of a is : " << modin(a, m, x, p) << '\n';
+    
+    ll ans = modin(a, m);
+    if(ans == -1){
+        cout << "Modular inverse doesn't exist\n";
+    } else {
+        cout << "MMI of a modulo m is : " << ans << '\n';
+    }
     
     return 0;
 }
